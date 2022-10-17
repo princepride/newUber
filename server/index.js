78f835939bcc5d51b06457ba1bc0b4f081cae2ff
汪志鹏 <13476859295@163.com>
2022-10-17 10:02:29 +0800

    add backend

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express()
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.put('/stockportfolio', (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const db = new sqlite3.Database('./uber.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        else {
+            console.log('Connected to the database.');
+        }
+    });
+    db.serialize(() => {
+        //db.each(`SELECT stocktickers,chromosome,max(sharpe) FROM garesults WHERE date='${day}'`, (err, row) => {
+        db.each(`SELECT firstname,lastname,gender,destination FROM userprofile WHERE firstname is ? AND lastname is ?`, 
+        [firstname, lastname],
+        (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            res.send(row)
+        });
+    });
+});
+
+app.listen(3001, () => {
+    console.log('listening on port 3001')
+})
